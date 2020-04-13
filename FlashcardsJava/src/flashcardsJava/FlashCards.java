26b09c0eb5b5f7280c07f640aac113f246f4f832
MikePTC <60409306+MikePTC@users.noreply.github.com>
2020-04-13 14:36:16 -0400

    Update FlashCards.java

--- FlashcardsJava/src/flashcardsJava/FlashCards.java
+++ FlashcardsJava/src/flashcardsJava/FlashCards.java
@@ -15,8 +15,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
